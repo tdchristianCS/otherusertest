c4ebcefcdc2434c065ea5d3b8d9755c42cd568bb
Mr <sawczak@tdchristian.ca>
2023-04-18 14:41:24 -0400

    problems

---
+++ sawczak_work/problems/celsius.py
@@ -0,0 +1,12 @@
+# write a program that takes a number in degrees Fahrenheit
+# and outputs the equivalent in degrees Celsius.
+
+# ====
+
+# e.g. if they enter 32
+# your program prints 0.0
+# if they enter -40
+# your program prints -40.0
+
+# ====
+
---
+++ sawczak_work/problems/smush_3.py
@@ -0,0 +1,16 @@
+# write a program that works like smush_names.py,
+# except it takes 3 names and combines them in thirds.
+
+# ====
+
+# Here is a sample set of inputs and output
+
+"""
+Enter a name: Robert
+Enter a name: Paul
+Enter a name: Yiwen
+Roaen
+"""
+
+# ====
+
