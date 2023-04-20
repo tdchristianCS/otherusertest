23c6d044b159f29bcd6b4a9116c4f9e960026d84
Mr <sawczak@tdchristian.ca>
2023-04-20 14:36:24 -0400

    random

---
+++ sawczak_work/what_is_random.py
@@ -0,0 +1,19 @@
+# How to have randomness
+
+import random
+
+# print(dir(random))
+# There are a lot...
+# The most useful 3...
+
+# random.choice picks a random item from a container
+names = ['Sean', 'Jean', 'Dean']
+print(random.choice(names))
+
+# random.shuffle shuffles the order of items in a container
+random.shuffle(names)
+print(names)
+
+# random.randint gives you a number between A and B inclusive
+n = random.randint(1, 100)
+print(n)
