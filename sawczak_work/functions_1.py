c6250aa46ed40731fa2dbd5de8f54f16d78f2517
Mr <sawczak@tdchristian.ca>
2023-05-09 14:55:39 -0400

    fns

--- sawczak_work/functions_1.py
+++ sawczak_work/functions_1.py
@@ -2,6 +2,11 @@
 def sqrt(n: int) -> float:    
     return n ** (1/2)
 
+# INVOKE
+for i in range(1, 10):
+    square_root = sqrt(i)
+    print(f'sqtr of {i}: {square_root}')
+
 # DEFINE
 def factorial(n: int) -> int:
 
@@ -15,7 +20,15 @@ def factorial(n: int) -> int:
             product = product * multiplier
         
         return product
+    
+# INVOKE
+for i in range(1, 10):
+    square_root = factorial(i)
+    print(f'factorial of {i}: {factorial}')
 
+# DEFINE
+# In this one, the """ ... """ thing is called a docstring
+# You write an example of how to use it and what you would expect to happen
 def remove_all(L: list, thing_to_remove: object) -> None:
     """
     Remove every copy of thing_to_remove from L.
@@ -31,14 +44,15 @@ def remove_all(L: list, thing_to_remove: object) -> None:
     
     return
 
+# TEST
+# When you have the docstring above, you can use doctest to run your tests
+# If you see nothing, it means your function passed the tests
+# Otherwise it will tell you what went wrong
 import doctest
 doctest.testmod()
 
 # INVOKE
-# for i in range(26):
-#     square_root = sqrt(i)
-#     print(f'sqtr of {i}: {square_root}')
-
-
-
+test_list = [3, 3, 3, 3, 4]
+remove_all(test_list)
+print(test_list)
 
